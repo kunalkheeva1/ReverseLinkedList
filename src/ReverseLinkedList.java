f4e13555dbe6686ed58a55725123c75be8110958
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-11 20:47:26 -0700

    coded the method to return the linked list

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -10,7 +10,24 @@ Node(int data, Node next){
 
 public class ReverseLinkedList {
 
+static Node reverseLinkedList(Node root){
 
+    if(root == null || root.next== null){
+        return root;
+    }
+    Node current = root;
+    Node next = null;
+    Node previous = null;
+
+    while(current != null){
+
+        next = current.next;
+        current.next= previous;
+        previous = current;
+        current = next;
+    }
+
+}
 
 
     public static void main(String[] args) {
