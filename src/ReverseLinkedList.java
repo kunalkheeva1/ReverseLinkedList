5a89ba4fd282ae135df476b88fd259495c5d6361
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-11 19:29:33 -0700

    coded a node class

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,4 +1,16 @@
+ class Node{
+    int data;
+Node next;
+Node(int data, Node next){
+    this.data = data;
+    this.next = null;
+}
+}
+
+
 public class ReverseLinkedList {
+
+
 
 
     public static void main(String[] args) {
